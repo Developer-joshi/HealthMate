69920704db6225018412a52b4d00c000f5c1988b
Shresth <142995647+spidy2301@users.noreply.github.com>
2025-06-15 18:17:15 +0530

    All Doctors according to spciality  page added

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,8 +3,8 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div className=' md:mx-10'>
-       <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+    <div className='md:mx-10'>
+       <div className='flex flex-auto sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
         <div>
             <img className='mb-5 w-40' src={assets.logo} alt="" />
             <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, incidunt molestias explicabo aspernatur veritatis laboriosam odit itaque ipsum consectetur illo numquam cupiditate facere vitae debitis?</p>
@@ -30,7 +30,7 @@ const Footer = () => {
     </div>
     <div>
         <hr />
-        <p className='py-5 text-sm text-center'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis dignissimos cupidita</p>
+        <p className='py-5 text-sm text-center'>Copyright 2025@ All Rights Reserverd</p>
     </div>
     </div>
   )
