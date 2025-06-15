5c630cc4fcda5ec0c188dd6661968390b949b45f
Developer-joshi <sarveshjoshi017@gmail.com>
2025-06-15 13:50:58 +0530

    backend folder structure created

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import { connectDB } from "./config/mongodb.js";
+import multer from "multer";
+import cloudinary from "cloudinary";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+// App config
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB()
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// API endpoints
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
