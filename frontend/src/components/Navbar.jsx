b7046e1e7c7f29d88ed136750956cf40c6d3f645
vishalnikhil <vishalnikhil2002@gmail.com>
2025-06-18 15:46:09 +0530

    made page responsive for mobile screen as well

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,39 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from '../assets/assets'
 import { NavLink, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
   const navigate = useNavigate();
-  const [token, setToken] = React.useState(true); // Only this is used
+  const [ShowMenu, SetShowMenu] = useState(false);
+  const [token, setToken] = React.useState(true);
 
   return (
     <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
-      <img onClick={()=>navigate('/')}  className="w-44 cursor-pointer" src={assets.logo} alt="" />
+      <img onClick={() => navigate('/')} className="w-44 cursor-pointer" src={assets.logo} alt="logo" />
       
       <ul className="hidden md:flex items-start gap-5 font-medium">
-        <NavLink to="/">
-          <li className="py-1">HOME</li>
-          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
-        </NavLink>
-        <NavLink to="/doctors">
-          <li className="py-1">ALL DOCTORS</li>
-          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
-        </NavLink>
-        <NavLink to="/about">
-          <li className="py-1">ABOUT</li>
-          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
-        </NavLink>
-        <NavLink to="/contact">
-          <li className="py-1">CONTACT</li>
-          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
-        </NavLink>
+        <NavLink onClick={() => SetShowMenu(false)} to="/"><li className="py-1">HOME</li></NavLink>
+        <NavLink onClick={() => SetShowMenu(false)} to="/doctors"><li className="py-1">ALL DOCTORS</li></NavLink>
+        <NavLink onClick={() => SetShowMenu(false)} to="/about"><li className="py-1">ABOUT</li></NavLink>
+        <NavLink onClick={() => SetShowMenu(false)} to="/contact"><li className="py-1">CONTACT</li></NavLink>
       </ul>
 
       <div className="flex items-center gap-4">
         {token ? (
           <div className='flex items-center gap-2 cursor-pointer group relative'>
-            <img className='w-8 rounded-full' src={assets.profile_pic} alt="" />
-            <img className='w-2.5' src={assets.dropdown_icon} alt="" />
+            <img className='w-8 rounded-full' src={assets.profile_pic} alt="profile" />
+            <img className='w-2.5' src={assets.dropdown_icon} alt="dropdown" />
             <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
               <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
                 <p onClick={() => navigate('my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
@@ -50,7 +39,22 @@ const Navbar = () => {
             Create Account
           </button>
         )}
-      </div>
+
+        <img onClick={() => SetShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon} alt="menu" />
+
+        {/* Mobile menu */}
+        <div className={`${ShowMenu ? 'fixed w-full h-full' : 'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
+          <img src={assets.logo} alt="logo" />
+          <img onClick={() => SetShowMenu(false)} src={assets.cross_icon} alt="close" />
+
+          <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
+            <NavLink  onClick={() => SetShowMenu(false)} to="/"><p className='px-4 py-2 rounded inline-block'>Home</p></NavLink>
+            <NavLink onClick={() => SetShowMenu(false)} to="/doctors"><p className='px-4 py-2 rounded inline-block'>All DOCTORS</p></NavLink>
+            <NavLink onClick={() => SetShowMenu(false)} to="/about"><p className='px-4 py-2 rounded inline-block'>ABOUT</p></NavLink>
+            <NavLink onClick={() => SetShowMenu(false)} to="/contact"><p className='px-4 py-2 rounded inline-block'>CONTACT US</p></NavLink>
+          </ul>
+        </div>
+      </div>  
     </div>
   );
 };
