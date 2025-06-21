cf933b74f4033aaa7483b7c5d8bc6ce216fe4b36
Shrivats Mishra <shrivats2004@gmail.com>
2025-06-21 08:27:44 +0530

    added routing for different pages in admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,8 +3,13 @@ import Login from './pages/login'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import {ToastContainer,toast} from 'react-toastify'
+import {Route,Routes} from 'react-router-dom'
 import 'react-toastify/dist/ReactToastify.css'
 import { AdminContext } from './context/AdminContext'
+import Dashboard from './pages/Admin/Dashboard'
+import AllAppointments from './pages/Admin/AllAppointments'
+import AddDoctor from './pages/Admin/AddDoctor'
+import DoctorsList from './pages/Admin/DoctorsList'
 const App = () => {
 
   const {aToken} = useContext(AdminContext);
@@ -14,6 +19,13 @@ const App = () => {
       <Navbar/>
       <div className='flex items-start'>
       <Sidebar/>
+      <Routes>
+      <Route path='/' element={<></>} />
+          <Route path='/admin-dashboard' element={<Dashboard />} />
+          <Route path='/all-appointments' element={<AllAppointments />} />
+          <Route path='/add-doctor' element={<AddDoctor />} />
+          <Route path='/doctor-list' element={<DoctorsList />} />
+      </Routes>
       </div>
     </div>
   ) : (
