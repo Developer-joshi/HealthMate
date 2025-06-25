6583128d7955a8f3e33792865579ff0f80b1a4e9
Shrivats Mishra <shrivats2004@gmail.com>
2025-06-25 08:52:56 +0530

    added api to fetch user appointment from frontend

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -2,8 +2,27 @@
   import { AppContext } from "../context/AppContext"
 
   const MyAppointments = () => {
-    const { doctors } = useContext(AppContext)
+    const { backendUrl, token } = useContext(AppContext)
+      
+    const[appointments,setAppointments]=useContext([])
 
+    const getUserAppointments = async()=>{
+      try{
+        const{data}=await axios.get(backendUrl+'/api/user/appointments',{headers:{token}})
+         
+        if(data.success){
+          setAppointments(data.appointments.reverse())
+        }
+      }catch(error){
+        console.log(error);
+        toast.error(error.message)
+      }
+    }
+    useEffect(()=>{
+      if(token){
+        getUserAppointments()
+      }
+    },[token])
     return (
       <div className="mt-12">
         <p className="pb-3 text-lg font-medium text-gray-600 border-b">
@@ -26,24 +45,23 @@
             
               <img
                 className="w-32 bg-indigo-50"
-                src={item.image}
-                alt={item.name}
+                src={item.docData.image}
+                alt={item.docData.name}
               />
 
             
               <div className="flex-1 text-sm text-xinc-600">
                 <p className="text-nuetral font-semibold">
-                  {item.name}
+                  {item.docData.name}
                 </p>
-                <p>{item.speciality}</p>
+                <p>{item.docData.speciality}</p>
                 <p className="text-zinc-700 font-medium mt-1">Address:</p>
-                <p className='text-xs'>{item.address.line1}</p>
-                <p className='text-xs'>{item.address.line2}</p>
+                <p className='text-xs'>{item.docData.address.line1}</p>
+                <p className='text-xs'>{item.docData.address.line2}</p>
                 <p className="text-xs mt-1">
                   <span className="text-sm text-neutral-700 font-medium">
                     Date &amp; Time:
-                  </span>{" "}
-                  18 June, 2025 | 8:30 PM
+                  </span>{item.slotDate}|{item.slotTime}
                 </p>
               </div>
 
