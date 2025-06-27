baa31d8eba223346e4dd0a06f76e8bf519d8bd16
Developer-joshi <sarveshjoshi017@gmail.com>
2025-06-28 01:31:59 +0530

    added doctor login

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -1,28 +1,55 @@
-// API to change doctor availablity for Admin and Doctor Panel
-import doctorModel from '../models/doctorModel.js'
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import doctorModel from "../models/doctorModel.js";
+import appointmentModel from "../models/appointmentModel.js";
+// API for doctor Login
+const loginDoctor = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await doctorModel.findOne({ email });
+
+    if (!user) {
+      return res.json({ success: false, message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);//from req and hashed passward in db
+
+    if (isMatch) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } 
+    else {
+      res.json({ success: false, message: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 const changeAvailablity = async (req, res) => {
-    try {
+  try {
+    const { docId } = req.body;
 
-        const { docId } = req.body
+    const docData = await doctorModel.findById(docId);
+    await doctorModel.findByIdAndUpdate(docId, {
+      available: !docData.available,
+    });
+    res.json({ success: true, message: "Availablity Changed" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
-        const docData = await doctorModel.findById(docId)
-        await doctorModel.findByIdAndUpdate(docId, { available: !docData.available })
-        res.json({ success: true, message: 'Availablity Changed' })
+const doctorList = async (req, res) => {
+  try {
+    const doctors = await doctorModel.find({}).select(["-password", "-email"]);
+    res.json({ success: true, doctors });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
-    } catch (error) {
-        console.log(error)
-        res.json({ success: false, message: error.message })
-    }
-}
-
-const doctorList = async (req,res)=>{
-    try {
-        const doctors = await doctorModel.find({}).select(['-password','-email'])
-        res.json({success:true,doctors})
-    } catch (error) {
-        console.log(error)
-        res.json({success:false,message:error.message})
-    }
-}
-export {changeAvailablity,doctorList}
+export { changeAvailablity, doctorList ,loginDoctor};
